af4b05cc8e5a58f82c900da2b0774e392bb9d6ad
flonberg <48604516+flonberg@users.noreply.github.com>
2026-06-26 10:48:45 -0400

    make beams list

--- src/app/get-rtbdi/get-rtbdi.component.ts
+++ src/app/get-rtbdi/get-rtbdi.component.ts
@@ -1,6 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+type Beam = {
+  CurrentFractionNumber: string;
+  ReferencedBeamNumber: string;
+  ReferencedFractionGroupNumber: string;
+};
+
 @Component({
   selector: 'app-get-rtbdi',
   standalone: true,
@@ -8,35 +14,47 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './get-rtbdi.component.html',
   styleUrl: './get-rtbdi.component.css'
 })
+
 export class GetRTBDIComponent {
   private http = inject(HttpClient); 
   phpData: any;
+  beams: Beam[] = [];
   ngOnInit() {
-    this.fetchPhpData();
+    this.fetchRTBCIData();
   }
 
-  fetchPhpData() {
-    // Replace with the actual URL where your PHP script is hosted
+  fetchRTBCIData() {
     const phpApiUrl = 'https://whiteboard.partners.org/esb/_dev_/dicom/testRTBDIjson.php'; 
     this.http.get<any>(phpApiUrl).subscribe({
       next: (response) => {
         this.phpData = response;
+        var beam = {} as Beam;  
+        var startBeam = false;
         console.log("type of phpData:", typeof this.phpData);
         for (const [key, value] of Object.entries(this.phpData)) {
-          if (this.canBeString(value) && String(value).includes('0074,1022')) {
+          if (typeof value !== 'symbol' && String(value).includes('0074,1022')) {
             console.log(`${key}: ${value}`);
+            beam = {} as Beam;
+            startBeam = true;
           }
+          if (typeof value !== 'symbol' && String(value).includes('3008,0022')) 
+              beam.CurrentFractionNumber = this.getStringInBrackets(String(value)) || '';
+          if (typeof value !== 'symbol' && String(value).includes('300c,0022')) 
+              beam.ReferencedFractionGroupNumber = this.getStringInBrackets(String(value)) || '';
+          if (typeof value !== 'symbol' && String(value).includes('300c,0006')) {
+              beam.ReferencedBeamNumber = this.getStringInBrackets(String(value)) || '';
+              this.beams.push(beam);
+          }
+   
+          
         }
+        console.log("beams:", this.beams);
       },
       error: (err) => console.error('Failed to reach PHP script:', err)
     });
   }
-  canBeString(value: unknown) {
-    if (typeof value !== 'symbol') {
-      const safeString = String(value); // Outputs "null", "123", "[object Object]"
-      return true
-      }
-    else
-    return false  
-    }
+  getStringInBrackets(input: string): string | null {
+    const match = input.match(/\[(.*?)\]/);
+    return match ? match[1] : null;
+  }
 }
